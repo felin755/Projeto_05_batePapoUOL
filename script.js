b70696b27a141202e3dea1fe7badfba0c01be051
felin755 <linfer755@gmail.com>
2022-04-17 09:53:38 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function enter() {
+    let pagInicial = document.querySelector(".tela-entrada")
+    pagInicial.classList.add("sumido")
+    console.log("clicou enter")
+}
+
+function envioMsg() {
+    alert("vc apertou o botao")
+}
